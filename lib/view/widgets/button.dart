6e2794e2409cefe681de9d6414eba482922a2dba
guicrisostomo <guilhermecrisostomo75@gmail.com>
2023-01-27 19:18:23 -0300

    feat: add new widget

---
+++ lib/view/widgets/button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hour/globals.dart' as globals;
+
+Widget button(String text, double width, double height, Function() onPressed) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(color: globals.primary, spreadRadius: 3),
+        ],
+      ),
+
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(width, height), 
+          backgroundColor: Colors.white70,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          )
+        ),
+        
+        onPressed: onPressed,
+        
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 24,
+            color: globals.primary,
+            fontWeight: FontWeight.w600,
+          )
+        ),
+
+      ),
+    );
+  }
