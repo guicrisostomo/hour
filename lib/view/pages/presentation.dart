a04d00dd889896e8807541e260fdf682b74bd1eb
guicrisostomo <guilhermecrisostomo75@gmail.com>
2023-01-27 19:22:52 -0300

    refactor: remove code not necessary

--- lib/view/pages/presentation.dart
+++ lib/view/pages/presentation.dart
@@ -16,54 +16,51 @@ class _PresentationState extends State<Presentation> {
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: Theme.of(context).copyWith(scaffoldBackgroundColor: globals.secundary),
-      child: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: const AssetImage(
-                "lib/images/background.jpg",
-              ),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(globals.primary.withOpacity(0.8), BlendMode.modulate),
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage(
+              "lib/images/background.jpg",
             ),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(globals.primary.withOpacity(0.8), BlendMode.modulate),
           ),
+        ),
 
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-                Image.asset('lib/images/iconMyHour.png', width: 200, height: 200),
+              Image.asset('lib/images/iconMyHour.png', width: 200, height: 200),
 
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text(
-                    'Controle seu tempo de maneira rápida e fácil',
-                    style: GoogleFonts.lato(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  'Controle seu tempo de maneira rápida e fácil',
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 20,
                   ),
                 ),
+              ),
 
-                const SizedBox(height: 50),
+              const SizedBox(height: 50),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-              
-                  children: [
-                    button('Cadastrar', 150, 50, () => Navigator.popAndPushNamed(context, '/register')),
-                    button('Entrar', 150, 50, () => Navigator.popAndPushNamed(context, '/login')),
-                    
-                  ],
-                ),
-              ]
-            ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.end,
+            
+                children: [
+                  button('Cadastrar', 150, 50, () => Navigator.popAndPushNamed(context, '/register')),
+                  button('Entrar', 150, 50, () => Navigator.popAndPushNamed(context, '/login')),
+                  
+                ],
+              ),
+            ]
           ),
         ),
       ),
