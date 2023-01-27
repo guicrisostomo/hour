52d31389784d26de6e840c1dc6cc38e7945ce111
guicrisostomo <guilhermecrisostomo75@gmail.com>
2023-01-27 19:09:49 -0300

    config: add dependency and new image

--- lib/view/pages/login.dart
+++ lib/view/pages/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hour/view/widgets/textFieldEmail.dart';
+import 'package:hour/view/widgets/textFieldPassword.dart';
+import 'package:hour/globals.dart' as globals;
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,13 +11,43 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  var txtEmail = TextEditingController();
+  var txtPassword = TextEditingController();
+
+  void logIn(a) {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+    return Theme(
+      data: Theme.of(context).copyWith(scaffoldBackgroundColor: globals.secundary),
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: const AssetImage(
+                "lib/images/background.jpg",
+              ),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(globals.primary.withOpacity(0.8), BlendMode.modulate),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                textFieldEmail('Email', txtEmail, context),
+              
+                const SizedBox(height: 10),
+              
+                TextFieldPassword(label: 'Senha', variavel: txtPassword, onFieldSubmitted: logIn),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
