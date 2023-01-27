322ae8e6c6e90dfa86d5f072dacafa75d7c9db0b
guicrisostomo <guilhermecrisostomo75@gmail.com>
2023-01-26 23:18:03 -0300

    chore: add global variables

---
+++ lib/globals.dart
@@ -0,0 +1,16 @@
+library globals;
+
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFF0B2B40);
+Color secundary = Color(0xFF30A5BF);
+Color tertiary = Color(0xFFF2BE22);
+
+// Maybe color
+/*
+  #0B2B40
+  #30A5BF
+  #185359
+  #F2BE22
+  #A6874E
+*/
