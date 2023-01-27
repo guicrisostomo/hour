cb25051e5030e5d69004729997081a0bd55f8aed
guicrisostomo <guilhermecrisostomo75@gmail.com>
2023-01-27 20:19:46 -0300

    feat: add elements to screen login

--- lib/view/pages/login.dart
+++ lib/view/pages/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hour/view/widgets/button.dart';
 import 'package:hour/view/widgets/textFieldEmail.dart';
 import 'package:hour/view/widgets/textFieldPassword.dart';
 import 'package:hour/globals.dart' as globals;
@@ -14,8 +16,8 @@ class _LoginState extends State<Login> {
   var txtEmail = TextEditingController();
   var txtPassword = TextEditingController();
 
-  void logIn(a) {
-    
+  void logIn() {
+    Navigator.popAndPushNamed(context, '/home');
   }
 
   @override
@@ -23,30 +25,85 @@ class _LoginState extends State<Login> {
     return Theme(
       data: Theme.of(context).copyWith(scaffoldBackgroundColor: globals.secundary),
       child: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: const AssetImage(
-                "lib/images/background.jpg",
+        body: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width,
+
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage(
+                    "lib/images/background.jpg",
+                  ),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(globals.primary.withOpacity(0.8), BlendMode.modulate),
+                ),
               ),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(globals.primary.withOpacity(0.8), BlendMode.modulate),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                textFieldEmail('Email', txtEmail, context),
+
+              child: Padding(
+                padding: const EdgeInsets.all(40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Entrar',
+                      style: GoogleFonts.lato(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
               
-                const SizedBox(height: 10),
+                    const SizedBox(height: 5),
               
-                TextFieldPassword(label: 'Senha', variavel: txtPassword, onFieldSubmitted: logIn),
-              ],
+                    Text(
+                      'Ainda não possui uma conta ?',
+                      style: GoogleFonts.lato(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: button('Criar conta', 100, 50, () => Navigator.popAndPushNamed(context, '/register'))
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
+
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 100),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      textFieldEmail('Email', txtEmail, context),
+                    
+                      const SizedBox(height: 10),
+                    
+                      TextFieldPassword(label: 'Senha', variavel: txtPassword, onFieldSubmitted: (value) => logIn()),
+
+                      const SizedBox(height: 20),
+
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: button('Entrar', 200, 50, logIn)
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ]
         ),
       ),
     );
