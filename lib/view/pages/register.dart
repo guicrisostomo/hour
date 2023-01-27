efaaf97696b54cafc735a821284333702c54acc5
guicrisostomo <guilhermecrisostomo75@gmail.com>
2023-01-26 23:20:20 -0300

    feat: add new screen

---
+++ lib/view/pages/register.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
