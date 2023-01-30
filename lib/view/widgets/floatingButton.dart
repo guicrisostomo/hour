5d5d7f366df9697507a8eda037ee6bc8bb726598
guicrisostomo <guilhermecrisostomo75@gmail.com>
2023-01-30 20:04:45 -0300

    feat: add widget floating button

---
+++ lib/view/widgets/floatingButton.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:hour/globals.dart' as globals;
+
+Widget floatingButton(context) {
+  return FloatingActionButton(
+    backgroundColor: globals.primary,
+    onPressed: () {
+      Navigator.pushNamed(context, 'cart');
+    },
+
+    child: const Icon(
+      Icons.calendar_month,
+      color: Colors.white,
+    ),
+  );
+}
